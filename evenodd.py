c8da7640c8214027e8d36cb1b03a2a41275ddcf8
Adityagattu1518 <gadityasai_cse230586@mgit.ac.in>
2025-09-02 15:04:02 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+# Even or Odd program in Python
+
+# Taking input from user
+num = int(input("Enter a number: "))
+
+# Checking condition
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
